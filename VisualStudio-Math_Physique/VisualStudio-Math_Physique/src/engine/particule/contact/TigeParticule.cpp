0df1988c92cfe29239cc9f1e4ec4e8583a16b27d
Sam-Carneiro <samcarneiro@outlook.fr>
2023-10-22 17:52:19 -0400

    fix
    la valeur de la longueurActuel() doit être la même que la longueur en paramètre

--- VisualStudio-Math_Physique/VisualStudio-Math_Physique/src/engine/particule/contact/TigeParticule.cpp
+++ VisualStudio-Math_Physique/VisualStudio-Math_Physique/src/engine/particule/contact/TigeParticule.cpp
@@ -9,7 +9,7 @@ TigeParticule::TigeParticule(Particule* particules[2], float longueur)
 
 void TigeParticule::ajouterContact(ContactParticules& contacts, unsigned int limit)
 {
-	if (contacts.size() < limit && longueurActuelle() > longueur)
+	if (contacts.size() < limit && longueurActuelle() == longueur)
 	{
 		contacts.push_back(new ContactParticule(particules, 1, longueurActuelle() - longueur, (particules[0]->position - particules[1]->position).direction()));
 	}
