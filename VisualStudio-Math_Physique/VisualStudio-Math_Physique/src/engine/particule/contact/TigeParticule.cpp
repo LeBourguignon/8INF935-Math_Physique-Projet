1f3cfc82befa8af8690edff7e932339b84b8a810
Sam-Carneiro <samcarneiro@outlook.fr>
2023-10-22 17:51:14 -0400

    fix
    longueur à été remis à sa place

--- VisualStudio-Math_Physique/VisualStudio-Math_Physique/src/engine/particule/contact/TigeParticule.cpp
+++ VisualStudio-Math_Physique/VisualStudio-Math_Physique/src/engine/particule/contact/TigeParticule.cpp
@@ -9,8 +9,8 @@ TigeParticule::TigeParticule(Particule* particules[2], float longueur)
 
 void TigeParticule::ajouterContact(ContactParticules& contacts, unsigned int limit)
 {
-	if (contacts.size() < limit)
+	if (contacts.size() < limit && longueurActuelle() > longueur)
 	{
-		contacts.push_back(new ContactParticule(particules, 1, longueur, (particules[0]->position - particules[1]->position).direction()));
+		contacts.push_back(new ContactParticule(particules, 1, longueurActuelle() - longueur, (particules[0]->position - particules[1]->position).direction()));
 	}
 }
